78755ab7bee325970e058927aa4a38b375c4f008
Valentin Vasilev <valentin7969@gmail.com>
2021-05-03 20:59:58 +0300

    Update search-bar.js

--- src/main/resources/static/js/search-bar.js
+++ src/main/resources/static/js/search-bar.js
@@ -3,7 +3,7 @@ const searchBar = document.getElementById('searchInput')
 
 const allProducts = [];
 
-fetch("http://localhost:8000/products/api").then(response => response.json()).then(data => {
+fetch("https://kozmetichno.herokuapp.com/products/api").then(response => response.json()).then(data => {
     for (let album of data) {
         allProducts.push(album);
     }
